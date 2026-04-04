9d52acc6b26bc60b68518efac087943947ee1c20
lucija <lucijasteler6@gmail.com>
2026-04-04 11:17:27 +0200

    add lv7 files

---
+++ LV7/Zadatak_2_template.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as Image
+from sklearn.cluster import KMeans
+
+# ucitaj sliku
+img = Image.imread("imgs\\test_1.jpg")
+
+# prikazi originalnu sliku
+plt.figure()
+plt.title("Originalna slika")
+plt.imshow(img)
+plt.tight_layout()
+plt.show()
+
+# pretvori vrijednosti elemenata slike u raspon 0 do 1
+img = img.astype(np.float64) / 255
+
+# transfromiraj sliku u 2D numpy polje (jedan red su RGB komponente elementa slike)
+w,h,d = img.shape
+img_array = np.reshape(img, (w*h, d))
+
+# rezultatna slika
+img_array_aprox = img_array.copy()
