6e8eb23128c198f87125462e0bc960f4bf30f747
lucija <lucijasteler6@gmail.com>
2026-04-12 14:03:27 +0200

    add lv8

---
+++ LV8/zadatak_1.py
@@ -0,0 +1,57 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# train i test podaci
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# prikaz karakteristika train i test podataka
+print('Train: X=%s, y=%s' % (x_train.shape, y_train.shape))
+print('Test: X=%s, y=%s' % (x_test.shape, y_test.shape))
+
+# TODO: prikazi nekoliko slika iz train skupa
+
+
+# skaliranje slike na raspon [0,1]
+x_train_s = x_train.astype("float32") / 255
+x_test_s = x_test.astype("float32") / 255
+
+# slike trebaju biti (28, 28, 1)
+x_train_s = np.expand_dims(x_train_s, -1)
+x_test_s = np.expand_dims(x_test_s, -1)
+
+print("x_train shape:", x_train_s.shape)
+print(x_train_s.shape[0], "train samples")
+print(x_test_s.shape[0], "test samples")
+
+
+# pretvori labele
+y_train_s = keras.utils.to_categorical(y_train, num_classes)
+y_test_s = keras.utils.to_categorical(y_test, num_classes)
+
+
+# TODO: kreiraj model pomocu keras.Sequential(); prikazi njegovu strukturu
+
+
+
+# TODO: definiraj karakteristike procesa ucenja pomocu .compile()
+
+
+
+# TODO: provedi ucenje mreze
+
+
+
+# TODO: Prikazi test accuracy i matricu zabune
+
+
+
+# TODO: spremi model
+
